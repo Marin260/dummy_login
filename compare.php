43b78acb976a3ba274c4439fcb0e810db4671ba7
Marinm260 <marin.martuslovic@gmail.com>
2020-09-20 16:36:32 +0200

    upadated ps

--- compare.php
+++ compare.php
@@ -29,40 +29,7 @@ if(mysqli_num_rows($result) == 0){
 }
 else{
     //username exists
-    exit("Username exists");
+    exit("Username or email exists");
 }
-
-
-
-
-//$exists = 0; //varijabla koja gleda postoji li vec korisnik
-
-/*if($exists == 0){
-    $sql = "INSERT INTO new_user (username, email, acc_pas)    
-        VALUES ('$username', '$email', '$acc_pas')";
-    
-        if (mysqli_query($conn, $sql)) {
-            echo 'New record created successfully<br><br><a href="index.php">Go back</a>';
-          } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-          }
-}*/
-/*
-if (mysqli_num_rows($result) > 0)
-    while($row = mysqli_fetch_assoc($result)){
-        if($row["username"] == $username){
-            echo '<br>This username is already taken<br><br><a href="index.php">Go back</a>';
-            $exists = 1;
-            break;
-        }
-        elseif($row["email"] == $email){
-            echo '<br>Email addres already in use<br><br><a href="index.php">Go back</a>';
-            $exists = 1;
-            break;
-        }
-    }
-*/
-
-//unos
 
 ?>
